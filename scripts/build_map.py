8b4510c39bbb4af5235674265ddc21c6032bd69c
ahansen092003-hue <ahansen092003@gmail.com>
2026-02-12 12:34:29 -0800

    build map

--- scripts/build_map.py
+++ scripts/build_map.py
@@ -1,12 +1,30 @@
 import osmnx as ox
-import folium  
+import folium
 
 def build_map(location):
-    
     mum_map = ox.graph_from_bbox(bbox=(location[0], location[1], location[2], location[3]), network_type='drive', simplify=True)
-    FM_mum_map = ox.plot_graph_folium(mum_map, popup_attribute='name', weight=2, color='blue')
-    FM_mum_map.save('map.html')
+    nodes, edges = ox.graph_to_gdfs(mum_map)
+    center_lat = (location[1] + location[3]) / 2  
+    center_lon = (location[0] + location[2]) / 2
+    m = folium.Map(location=[center_lat, center_lon], zoom_start=14)
     
+    for idx, row in edges.iterrows():
+        folium.PolyLine(
+            locations=[(coord[1], coord[0]) for coord in row['geometry'].coords],
+            color='blue',
+            weight=2
+        ).add_to(m)
+
+    for idx, row in nodes.iterrows():
+        folium.CircleMarker(
+            location=[row['y'], row['x']],
+            radius=3,
+            color='red',
+            popup=str(idx)
+        ).add_to(m)
+        
+    m.save('map.html')
+
 if __name__ == "__main__":
-    worli_coords = (19.0167, 18.9778, 72.8389, 72.8056)
+    worli_coords = (72.8056, 18.9778, 72.8389, 19.0167)
     build_map(worli_coords)
