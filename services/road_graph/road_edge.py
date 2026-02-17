82da7c5e83a1c1b48da10d80c66a811dbcc4fe9e
ahansen092003-hue <ahansen092003@gmail.com>
2026-02-16 18:45:45 -0800

    fin road edge and begin new script to test

--- services/road_graph/road_edge.py
+++ services/road_graph/road_edge.py
@@ -1,4 +1,5 @@
 from collections import deque
+from services.traffic_sim.emergency_veh import Emergency_Veh
 
 class RoadEdge:
     def __init__(self, start_node, end_node, length, speed_limit):
@@ -20,6 +21,51 @@ class RoadEdge:
             del self.positions[vehicle]
             
     def update_positions(self, time_step):
-        for vehicle in list(self.vehicle_queue):
-            
-            pass
+        vehicles_list = list(self.vehicle_queue)
+        for vehicle in vehicles_list:
+            current_position = self.positions[vehicle]
+            if isinstance(vehicle, Emergency_Veh):
+                speed = self.speed_limit * 1.5
+            else:
+                speed = self.speed_limit
+                
+            desired_distance = speed * time_step
+            vehicle_index = vehicles_list.index(vehicle)
+            if vehicle_index > 0:
+                vehicle_ahead = vehicles_list[vehicle_index - 1]
+                ahead_position = self.positions[vehicle_ahead]
+                
+                if hasattr(vehicle_ahead, 'bus_id'):
+                    ahead_length = 10.0
+                elif hasattr(vehicle_ahead, 'car_id'):
+                    ahead_length = 5.0
+                elif hasattr(vehicle_ahead, 'emV_id'):
+                    ahead_length = 6.0
+                    
+                if isinstance(vehicle, Emergency_Veh):
+                    min_gap = 2
+                else:
+                    min_gap = 5
+                    
+                max_position = ahead_position - ahead_length - min_gap
+            else:
+                max_position = self.length
+                
+            self.positions[vehicle] = min(current_position + desired_distance, max_position)
+            if hasattr(vehicle, 'position'):
+                vehicle.position = self.positions[vehicle]
+                
+    def get_vehicle_counts(self):
+        cars = 0
+        buses = 0
+        emergency = 0
+    
+        for vehicle in self.vehicle_queue:
+            if hasattr(vehicle, 'bus_id'):
+                buses += 1
+            elif hasattr(vehicle, 'emV_id'):
+                emergency += 1
+            elif hasattr(vehicle, 'car_id'):
+                cars += 1
+    
+        return {'cars': cars, 'buses': buses, 'emergency': emergency}
