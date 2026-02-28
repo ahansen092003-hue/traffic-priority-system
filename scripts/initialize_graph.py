76ed0a99eeabf16008e27311b758ccee6ab40b48
ahansen092003-hue <ahansen092003@gmail.com>
2026-02-27 21:17:43 -0800

    helper func

--- scripts/initialize_graph.py
+++ scripts/initialize_graph.py
@@ -15,6 +15,33 @@ def split_and_create_edges(path, graph, signal_list, road_edges):
     segment_start = path[0]
     segment_nodes = [path[0]]
     
+    for node in path[1:]: 
+        segment_nodes.append(node)
+    
+        if node in signal_list:
+            edge_id = (segment_start, node)
+            
+            if edge_id not in road_edges:
+                total_length = 0
+                edge_data = None
+                for i in range(len(segment_nodes) - 1):
+                    u = segment_nodes[i]
+                    v = segment_nodes[i + 1]
+                    edge_data = graph[u][v][0] 
+                    total_length += edge_data['length']
+                
+                speed_limit = 11.0  
+                if edge_data and 'maxspeed' in edge_data:
+                    try:
+                        speed_limit = int(edge_data['maxspeed']) / 3.6  # Convert km/h to m/s
+                    except (ValueError, TypeError):
+                        pass
+                road_edge = RoadEdge(segment_start, node, total_length, speed_limit)
+                road_edges[edge_id] = road_edge
+                
+            segment_start = node
+            segment_nodes = [node]
+    
 def build(location):
     mum_map = ox.graph_from_bbox(bbox=(location[0], location[1], location[2], location[3]), network_type='drive', simplify=True)
     
