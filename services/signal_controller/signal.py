4de122da486c55b449a60867d4a7c0d53b966c72
ahansen092003-hue <ahansen092003@gmail.com>
2026-03-02 12:14:49 -0800

    start from here

--- services/signal_controller/signal.py
+++ services/signal_controller/signal.py
@@ -7,12 +7,12 @@ class Signal:
         self.time_remaining = None
         self.car_num = None
         self.bus_num = None
-        self.north = bool
-        self.south = bool
-        self.east = bool
-        self.west = bool
-        self.bus_priority = bool
-        self.emergency_priority = bool
+        self.north = False
+        self.south = False
+        self.east = False
+        self.west = False
+        self.bus_priority = False
+        self.emergency_priority = False
         
     def distance_to_vehicle(self, vehicle_position):
         pass
